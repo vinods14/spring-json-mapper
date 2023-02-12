ea4bea6deabbea3243343ed850428eecf00b2fdf
Vinod Sharma <vinodsharma@Vinods-MacBook-Pro.local>
2023-02-13 00:08:10 +0100

    Added access specified on class

--- src/test/java/com/rkt/mapper/service/MapperServiceTest.java
+++ src/test/java/com/rkt/mapper/service/MapperServiceTest.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-class MapperServiceTest {
+public class MapperServiceTest {
 
     private MapperService mapperService;
 
